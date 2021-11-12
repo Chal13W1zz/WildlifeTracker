7e671d324aa33c8f19b345c14b751bf81aca5b27
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-12 10:51:24 +0300

    add a sighting details view

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,28 +17,40 @@ public class App {
             model.put("allsightings",allsightings);
             List<Animal> animalsInSighting = Sighting.getAnimalsInSighting();
             model.put("animalsInSighting",animalsInSighting);
-            System.out.println(allsightings.get(0).getRangerName());
+           // System.out.println(allsightings.get(0).getRangerName());
             return new ModelAndView(model,"index.hbs");
         },new HandlebarsTemplateEngine());
 
-        get("/new/sighting",(request, response)->{
+        //display new sighting form
+        get("/sighting/new",(request, response)->{
             Map<String, Object>model = new HashMap<>();
             return new ModelAndView(model,"new-sighting.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //process new sighting
         post("sighting/create",(request, response)->{
             String rangerName = request.queryParams("rangerName");
             boolean endangered = Boolean.parseBoolean(request.queryParams("endangered"));
             String location = request.queryParams("location");
             Sighting newsighting = new Sighting(rangerName,endangered,location);
             newsighting.saveSighting();
-            String animalName = request.queryParams("animalNames");
+            String animalName = request.queryParams("animalName");
             String health = request.queryParams("health");
             String animalAge = request.queryParams("animalAge");
             Animal newAnimal = new Animal(animalName,health,animalAge, newsighting.getId());
             newAnimal.saveAnimal();
             response.redirect("/");
             return null;
+        },new HandlebarsTemplateEngine());
+
+        get("/animal/:id",(request, response)->{
+            Map<String, Object>model = new HashMap<>();
+            Integer id = Integer.parseInt(request.params("id"));
+            Animal foundAnimal = Animal.findAnimalById(id);
+            Sighting foundSighting = Sighting.findSightingById(foundAnimal.getSightingId());
+            model.put("sighting",foundSighting);
+            model.put("animal",foundAnimal);
+            return new ModelAndView(model,"sighting.hbs");
         },new HandlebarsTemplateEngine());
     }
 }
